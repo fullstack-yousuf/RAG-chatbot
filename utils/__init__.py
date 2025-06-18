7da08bd73ee1400cce078a7c14f298c84b249aa9
Muhammad Yousuf <133602127+fullstack-yousuf@users.noreply.github.com>
2025-06-18 23:01:35 +0500

    update 3 for streamlitt

--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# Replace relative import with either:
-from .utils.generation import ResponseGenerator  # Relative import
-# OR (better for Streamlit Cloud)
-from utils.generation import ResponseGenerator  # Absolute import
